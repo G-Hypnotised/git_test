23af4ecd60e0e9dd38049d5446de2d9119e8a08f
PtrLiGo <237634500@qq.com>
2019-11-06 17:29:06 +0800

    新建User.java，提交到本地仓库

---
+++ git_test/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,15 @@
+package com.itheima.domain;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author lg
+ * @Company
+ * @Date 2019/11/6 16:38
+ * @Version V1.0
+ */
+public class User {
+
+    private Integer id;
+    private String name;
+}
